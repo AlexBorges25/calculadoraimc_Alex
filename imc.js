09f9c3f4556076bfe116d5bdd3c412acec386316
thiagoizidorio89 <marinheirocriacoes@gmail.com>
2021-06-02 20:41:31 -0300

    comentários adicionados sobre os erros encontrados

--- imc.js
+++ imc.js
@@ -1,7 +1,12 @@
+/* O correto é getElementById */
 var nome = document.ElementByID('nome');
 var altura = document.ElementByID('altura');
 var peso = document.ElementByID('peso');
+
+/*calcular é o botão então não deveria esta arqui*/
 var calcular = document.ElementByID('calcular');
+
+/*resultado não pode receber valor assim o valor dele vai ser o calculo do IMC*/
 var resultado = document.ElementByID('resultado');
 
 alert('aaa');
