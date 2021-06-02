eae8cf0abb5c38f5c14c87e89686bfb25e08e84c
AlexBorges25 <alexfnxborges@gmail.com>
2021-06-02 20:16:29 -0300

    html e js

---
+++ imc.js
@@ -0,0 +1,7 @@
+var nome = document.ElementByID('nome');
+var altura = document.ElementByID('altura');
+var peso = document.ElementByID('peso');
+var calcular = document.ElementByID('calcular');
+var resultado = document.ElementByID('resultado');
+
+alert('aaa');
